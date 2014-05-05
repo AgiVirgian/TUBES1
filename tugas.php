5ada5e51d71f6e51eaba81514c800d54895f3ab9
Agi Virgian <virgian_agi@yahoo.com>
2014-05-04 21:09:53 -0700

    edit2
    edit2

--- tugas.php
+++ tugas.php
@@ -23,11 +23,23 @@
  <br>
  <h1>Parsing dari situs web http://techno.okezone.com/breaking/56 </h1>
  <?php
-   $file = 'http://techno.okezone.com/breaking/56';
-   $doc = new DOMDocument();
-   $doc->loadHTMLFile($file);
-   echo $doc;
-  ?>
+$file ="http://techno.okezone.com/breaking/56";
+$doc = new DOMDocument();
+$doc->loadHTMLFile($file);
+
+$xpath = new DOMXpath($doc);
+$elements = $xpath->query("//*[@id]");
+if (!is_null($elements)) {
+  foreach ($elements as $element) {
+    echo "<br/>[". $element->nodeName. "]";
+
+    $nodes = $element->childNodes;
+    foreach ($nodes as $node) {
+      echo $node->nodeValue. "\n";
+    }
+  }
+}
+?>
  
  </div>
  <div id="footer">
